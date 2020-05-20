412ddf5e063eebb5729285ba2233a90c00577b24
okrzyzos <okrzyzos@outlook.fr>
2020-05-20 10:27:23 +0200

    modif6

--- index.php
+++ index.php
@@ -21,9 +21,50 @@ for ($i=0; $i < 10; $i++)
 }
 
 
-
 $nom = "dupont";
 $prenom = "olivier";
  
 echo  $prenom." ".$nom;
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/style.css" rel="stylesheet"/>
+
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+<ul>
+	<li>fruits</li>
+		<ul>
+			<li>pommes</li>
+			<li>poires</li>
+			<li>banane</li>
+                               
+		</ul>
+</ul>
+	<li>vehicules</li>
+		<ul>
+			<li>voiture</li>
+			<li>moto</li>
+			<li>avion</li>
+                               
+		</ul>
+</ul>
+	<li>pays</li>
+		<ul>
+			<li>france</li>
+			<li>bresil</li>
+			<li>belgique</li>
+                               
+		</ul>
+</ul>
