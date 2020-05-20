d1365910f21612e1491d3c99292c0b6e6f6e2659
okrzyzos <okrzyzos@outlook.fr>
2020-05-20 14:49:42 +0200

    function

--- function.php
+++ function.php
@@ -3,22 +3,17 @@
 
 
 
-function nombre(){
+function affichernombre($nb){
 
-$min = 0;
-$max = 100;
-$chiffres_pairs = 0;
-$i = 0;
  
-        for ($i = 0; $i < 100; $i++)
+        for ($i = 0; $i < $nb; $i++)
         {
-            $chiffres_pairs = 2*$i;
+
+            echo $i;
         }
  
-echo "Les chiffres pairs compris entre 0 et 100 sont: $chiffres_pairs";
 
     }
 
-nombre();
 
 ?>
